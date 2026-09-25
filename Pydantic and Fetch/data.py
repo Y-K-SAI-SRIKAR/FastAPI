07cb9b082a0ba289eed9efe9e2d640eb35bf5a3b
Y-K-SAI-SRIKAR <saisrikaryerraguntla@gmail.com>
2026-09-25 22:13:06 +0530

    Updated with FetchByID and POST methods

--- Pydantic and Fetch/data.py
+++ Pydantic and Fetch/data.py
@@ -3,8 +3,8 @@ from model import Products
 products=[
     Products(Id=1,Name="Laptop",Desc="A budget Laptop",Price=56000,Qty=2),
     Products(Id=2,Name="Mobile",Desc="Oppo Mobile",Price=26000,Qty=5),
-    Products(Id=1,Name="Iphone13",Desc="An Apple Product",Price=65000,Qty=3),
-    Products(Id=1,Name="Tablet",Desc="Xiaomi Tablet",Price=35000,Qty=6)
+    Products(Id=3,Name="Iphone13",Desc="An Apple Product",Price=65000,Qty=3),
+    Products(Id=4,Name="Tablet",Desc="Xiaomi Tablet",Price=35000,Qty=6)
 ]
 
 
